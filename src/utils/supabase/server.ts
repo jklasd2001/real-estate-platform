983b1cefcd8ba199ba11a4ee738d7e7a2f25832f
jklasd2001 <jklasd2001@gmail.com>
2025-09-12 23:49:16 +0900

    feat: 라이브러리 업데이트

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -5,6 +5,7 @@ import { env } from 'src/env'
 
 export async function createClient() {
   const cookieStore = await cookies()
+
   return createServerClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
     env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
@@ -15,9 +16,9 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            )
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options)
+            })
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
